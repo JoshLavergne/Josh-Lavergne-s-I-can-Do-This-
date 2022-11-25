30f2489dbed6388fb50b8757dee3cb7a096dd252
JoshLavergne <113380975+JoshLavergne@users.noreply.github.com>
2022-11-25 14:18:16 -0500

    Fixed messed up project

---
+++ ICanDoThis/src/OPERATOR.java
@@ -0,0 +1,21 @@
+import java.util.scanner;
+public class Main {
+
+	public static void main(string[] args) {
+
+		//Give the user ability to enter any number
+		
+	System.out.println("Enter any Number (Please do not use decimals)");
+	
+    double UserNumber = in.nextDouble();.
+    
+    double StepOne = (UserNumber-1);
+    double StepTwo = (StepOne*3);
+    double StepThree = (StepTwo+12);
+    double StepFour = (StepThree/3);
+    double StepFive = (StepFour+5);
+    double StepSix = (StepFive-UserNumber);
+    
+    System.out.println(StepSix);
+	}
+}
